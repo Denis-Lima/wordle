a3f28e710e340526ebf129ab40a12da6b74113e7
Denis Lima <denislima99@gmail.com>
2024-03-24 17:48:54 -0300

    Add style to the page

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -17,5 +17,34 @@ const guessSubmitted = ref("")
 
 <template>
   <GuessInput @guess-submitted="guessSubmitted = $event" />
-  <p v-if="guessSubmitted.length > 0" v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE" />
+  <p v-if="guessSubmitted.length > 0" class="end-of-game-message"
+    v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE" />
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3rem;
+}
+
+.end-of-game-message {
+  font-size: 3rem;
+  animation: end-of-game-message-animation 700ms forwards;
+  white-space: nowrap;
+  text-align: center;
+}
+
+@keyframes end-of-game-message-animation {
+  0% {
+    opacity: 0;
+    transform: rotateZ(0);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(2rem);
+  }
+}
+</style>
