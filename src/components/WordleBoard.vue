cdf7ab2a791ef926207ccc43f0ecd3dec3023838
Denis Lima <denislima99@gmail.com>
2024-03-24 17:30:13 -0300

    Fix bug that allowed palyer to type guesses with numbers

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { DEFEAT_MESSAGE, VICTORY_MESSAGE, WORD_SIZE } from '@/settings';
 import englishWords from '@/englishWordsWith5Letters.json'
-import { ref, computed } from 'vue'
+import { ref, watch } from 'vue'
 
 defineProps({
   wordOfTheDay: {
@@ -10,25 +10,18 @@ defineProps({
   }
 })
 
-const guessInProgress = ref("")
+const guessInProgress = ref<string>("")
 const guessSubmitted = ref("")
-
-const formattedGuessInProgress = computed({
-  get() {
-    return guessInProgress.value
-  },
-  set(rawValue: string) {
-    guessInProgress.value = rawValue.slice(0, WORD_SIZE).toUpperCase().replace(/[^A-Z]+/gi, '')
-  }
-})
 
 function onSubmit() {
   if (!englishWords.includes(guessInProgress.value)) return
   guessSubmitted.value = guessInProgress.value
 }
+
+watch(guessInProgress, v => { guessInProgress.value = v.slice(0, WORD_SIZE).toUpperCase().replace(/[^A-Z]+/gi, '') })
 </script>
 
 <template>
-  <input type="text" v-model="formattedGuessInProgress" @keydown.enter="onSubmit" maxlength="WORD_SIZE">
+  <input type="text" v-model="guessInProgress" @keydown.enter="onSubmit" maxlength="WORD_SIZE">
   <p v-if="guessSubmitted.length > 0" v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE" />
 </template>
