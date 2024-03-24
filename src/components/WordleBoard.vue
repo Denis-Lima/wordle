271e8aa1329ab1cd7748f92792500bc3c6975063
Denis Lima <denislima99@gmail.com>
2024-03-24 15:42:49 -0300

    Enforce word of the day to have 5 letters

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from '@/settings';
+import { ref } from 'vue'
+
+defineProps({
+  wordOfTheDay: {
+    type: String,
+    validator: (wordGiven: string) => wordGiven.length === 5
+  }
+})
+
+const guessInProgress = ref("")
+const guessSubmitted = ref("")
 </script>
 
 <template>
-  <p>{{ msg }}</p>
+  <input type="text" v-model="guessInProgress" @keydown.enter="guessSubmitted = guessInProgress">
+  <p v-if="guessSubmitted.length > 0" v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE" />
 </template>
--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,9 +1,43 @@
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
+import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from '@/settings'
 
 describe('WordleBoard', () => {
-  it('renders properly', () => {
-    const wrapper = mount(WordleBoard, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+  const wordOfTheDay = "TESTS"
+  let wrapper: ReturnType<typeof mount>
+  beforeEach(() => {
+    wrapper = mount(WordleBoard, { props: { wordOfTheDay: wordOfTheDay } })
+  })
+
+  async function playerSubmitsGuess(guess: string) {
+    const guessInput = wrapper.find("input[type=text]")
+    await guessInput.setValue(guess)
+    await guessInput.trigger("keydown.enter")
+  }
+
+  test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
+    await playerSubmitsGuess(wordOfTheDay)
+
+    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
+  })
+
+  test('a defeat message appears if the user makes a guess that is incorrect', async () => {
+    await playerSubmitsGuess("WRONG")
+
+    expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+  })
+
+  test('no end-of-game message appears if the user has not yet made a guess', async () => {
+    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
+    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
+  })
+
+  test('If a word of the day provided does not have exactly 5 characters, a warning is emitted', async () => {
+    vi.spyOn(console, "warn")
+
+    mount(WordleBoard, { props: { wordOfTheDay: 'FLY' } })
+
+    expect(console.warn).toHaveBeenCalled()
   })
 })
+
